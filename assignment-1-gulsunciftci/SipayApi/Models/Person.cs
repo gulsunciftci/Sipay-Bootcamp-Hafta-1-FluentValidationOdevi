7022f619ef5a928b57f1ab239d19b6454711bc0c
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-07-17 16:48:29 +0300

    fluent validation'a eklemeler yapıldı ve yeni öğrenilen bilgilerle bu kısım yeniden düzenlendi

--- assignment-1-gulsunciftci/SipayApi/Models/Person.cs
+++ assignment-1-gulsunciftci/SipayApi/Models/Person.cs
@@ -6,19 +6,10 @@ namespace SipayApi.Models
     public class Person
     {
         
-
         public string Name { get; set; }
-
-    
         public string LastName { get; set; }
-
-       
-        public DateTime Phone { get; set; }
-
-       
+        public string Phone { get; set; }
         public int AccessLevel { get; set; }
-
-
         public decimal Salary { get; set; }
 
     }
